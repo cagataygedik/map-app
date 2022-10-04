722247f93070010565387cb0b66769a2952a7af8
cagatay <cagatay.gedik@icloud.com>
2022-10-04 13:48:36 +0300

    Initial Commit

---
+++ map-app/map_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_appApp.swift
+//  map-app
+//
+//  Created by Celil Çağatay Gedik on 4.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct map_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
