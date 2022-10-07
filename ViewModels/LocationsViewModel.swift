090748691da3f0e6122b7303f255a1992e1b608b
cagatay <cagatay.gedik@icloud.com>
2022-10-07 14:38:12 +0300

    added nextButtonPressed function

--- ViewModels/LocationsViewModel.swift
+++ ViewModels/LocationsViewModel.swift
@@ -20,8 +20,12 @@ class LocationsViewModel: ObservableObject {
         }
     }
     
+    //Current Region
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    //SHow list of locations
+    @Published var showLocationsList: Bool = false
     
     init() {
         let locations = LocationsDataService.locations
@@ -34,8 +38,47 @@ class LocationsViewModel: ObservableObject {
     private func updateMapRegion(location: Location) {
         withAnimation(.easeInOut) {
             mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
-
+            
         }
+    }
+    
+    func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList.toggle()
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
+        }
+    }
+    
+    func nextButtonPressed() {
+        
+        //Get the current index
+        guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation}) else {
+            print("Could not find the currrent index in locations array")
+            return
+        }
+        
+        //check if the next index is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            //Next index is not valid
+            
+            
+            //restart from zero
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        
+        // next index is valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
     }
     
 }
